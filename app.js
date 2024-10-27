d8d42dcf33cd87bda08c1120fd97fbb09e456704
Sonephet <sonephetmnlv@gmail.com>
2024-10-27 14:56:39 +0700

    test api

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get("/", function (req, res) {
   res.send("Hello World");
 });
 
+app.get("/v1", function (req, res) {
+  res.send("Hello World v1");
+});
+
 app.listen(4500, () => {
   console.log("Server is running on port 4500");
 });
